721de6ead3e60e887b00828229399f9368709a07
dragon-web <1582943265@qq.com>
2020-08-21 17:14:18 +0800

    Aug_21

---
+++ Aug_21/test1/test.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+
+int main()
+{
+    system("/home/flf/linux_review/Aug_21/test1/demo1");
+    system("/home/flf/linux_review/Aug_21/test1/1");
+
+  return 0;
+}
