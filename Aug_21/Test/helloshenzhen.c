679e759593fcff7e7489677c3199381f14561fe9
dragon-web <1582943265@qq.com>
2020-08-21 16:59:39 +0800

    Aug_21

---
+++ Aug_21/Test/helloshenzhen.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{ 
+  printf("hello shenzhen\n");
+  return 0;
+}
---
+++ Aug_21/Test/te/main.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+  system("/home/flf/linux_review/Aug_21/Test/te/a.out");
+  return 0;
+}
