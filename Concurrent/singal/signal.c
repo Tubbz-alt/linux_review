164c75a86d40fc54b98ade972ae530767ce6c974
dragon-web <1582943265@qq.com>
2020-09-09 15:39:38 +0800

    Process/

--- Concurrent/singal/signal.c
+++ Concurrent/singal/signal.c
@@ -3,9 +3,19 @@
 #include<stdlib.h>
 #include<time.h>
 #include<sys/types.h>
+
+static void Inturput_Handlle(int s)
+{
+  write(1,"!",1);
+}
+
+
+
 int main()
 {
   int i;
+  signal(SIGINT,Inturput_Handlle);
+  //signal(SIGINT,)
   for(i = 0;i < 10;++i)
   {
     write(1,"*",1);
