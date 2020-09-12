e781b2ea6550fa0ee9e40ffc1c77e9853315540f
dragon-web <1582943265@qq.com>
2020-09-12 17:08:25 +0800

    Thread/

--- Thread/test.c
+++ Thread/test.c
@@ -9,6 +9,12 @@
 #define LINESIZE 1024
 char linebuf[LINESIZE];
 #define FNAME "/home/flf/linux_review/Thread/temp"
+
+static pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
+
+
+
+
 static void * thr_add(void *p)
 {
   FILE *fp;
@@ -18,16 +24,18 @@ static void * thr_add(void *p)
     perror("Fopen Error");
     exit(1);
   }
-  fseek(fp,0,SEEK_SET);
+  pthread_mutex_lock(&mut);
   fgets(linebuf,LINESIZE,fp);
-  fprintf(fp,"%d\n",atoi(linebuf)+1);
+  fseek(fp,0,SEEK_SET);
+  sleep(1);
+  fprintf(fp,"%d",atoi(linebuf)+1);
   
   fclose(fp);
-
+  pthread_mutex_unlock(&mut);
   pthread_exit(NULL);
 }
 
-
+//竞争看不出来问题可以将竞争放大   sleep
 
 int main()
 {
@@ -48,7 +56,6 @@ int main()
   {
     pthread_join(tid[i],NULL);
   }
-
 
   exit(0);
 }
