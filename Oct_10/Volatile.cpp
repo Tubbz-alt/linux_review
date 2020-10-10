2f713a61b230bd56ac79b9d72f1cad214080bd28
dragon-web <1582943265@qq.com>
2020-10-10 16:16:50 +0800

    Oct_10/

---
+++ Oct_10/Volatile.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+
+using namespace std;
+
+class Comm 
+{
+  private:
+    const volatile unsigned char byte;
+    volatile unsigned char flag;
+    enum{bufsize = 100};
+    unsigned char buf[bufsize];
+    int index;
+  public:
+    Comm();
+    void isr() volatile;
+    char read(int index) const;
+};
+
+Comm::Comm():index(0),byte(0),flag(0)
+{}
+
+void Comm::isr() volatile{
+  flag = 0;
+  buf[index++] = byte;
+  if(index >= bufsize)
+    index = 0;
+}
+
+char Comm::read(int index) const 
+{
+ if(index < 0 || index >= bufsize)
+   return 0;
+ return buf[index];
+}
+
+int main()
+{
+  volatile Comm Port;
+  Port.isr();
+}
