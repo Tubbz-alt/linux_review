100b3c639e745c4a10302c31dd93b38795d9c5d5
dragon-web <1582943265@qq.com>
2020-10-11 19:23:43 +0800

    Sort/

--- Sort/Insert_Sort/Direct_Insert_Sort/Dir_InsertSort.cpp
+++ Sort/Insert_Sort/Direct_Insert_Sort/Dir_InsertSort.cpp
@@ -4,19 +4,32 @@ using namespace std;
 
 void Dir_Insert_Sort(int arr[],int length)
 {
-
-
-
+  for(int i = 2;i <= length;++i)
+  {
+    int j = i-1;
+    arr[0] = arr[i];
+    while(arr[0] < arr[j])
+    {
+      arr[j+1] = arr[j];
+      j--;
+    }
+  arr[j+1] = arr[0];
+  }
 }
 
 int main()
 {
-  int arr[10];
-  for(int i = 0;i < 10;i++)
+  int arr[11];
+  for(int i = 0;i < 11;i++)
   {
     arr[i] = rand() % 10;
   }
-  Dir_Insert_Sort(arr,10);
+  for(auto &e : arr)
+  {
+    cout<<e<<" ";
+  }
+  cout<<endl;
+  Dir_Insert_Sort(arr,11);
   for(auto &e : arr)
   {
     cout<<e<<" ";
