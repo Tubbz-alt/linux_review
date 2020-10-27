34cf0ff54722fe6e5e9f51c67a073d046409b989
dragon-web <1582943265@qq.com>
2020-10-27 19:02:42 +0800

    ShellSort/

---
+++ Sort/ShellSort/ShellSort.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+
+// d = 4 d = 2 d = 1  
+
+void Shell_Sort(int arr[],int n)
+{
+  int d;
+  for(d = n /2 ; d >= 1 ; d = d/2)
+  {
+
+  }
+}
+
+int main()
+{
+  int arr[] = { 8, 1, 14, 3, 21, 5, 7, 10 , 8 , 7 };
+
+  return 0;
+}
